bf20a6130ab3b6b220f64c106ab131884416a2c1
Real <swrely@qq.com>
2022-10-31 09:30:14 +0800

    feat: add Item doc

--- src/builder/Item.java
+++ src/builder/Item.java
@@ -5,9 +5,24 @@ package builder;
  * @since 2022-10-29 12:24
  */
 public interface Item {
+    /**
+     * 命名
+     *
+     * @return 名称
+     */
     public String name();
 
+    /**
+     * 包装
+     *
+     * @return 打包接口类
+     */
     public Packing packing();
 
+    /**
+     * 价格
+     *
+     * @return 价格
+     */
     public float price();
 }
