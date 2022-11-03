edb30f5f48d3c7495be22755ea184d39f1b99288
Real <swrely@qq.com>
2022-11-03 19:16:06 +0800

    docs: add description of "builder" pattern

--- src/builder/BuilderPattern.java
+++ src/builder/BuilderPattern.java
@@ -4,6 +4,9 @@ import builder.builder.Meal;
 import builder.builder.MealBuilder;
 
 /**
+ * 将一个复杂的构建与其表示相分离，使得同样的构建过程可以创建不同的表示。
+ * 提供各种不同的可选项，但是可以实现不同的建造过程，得到多种建造结果。
+ *
  * @author wei.song
  * @since 2022-11-03 18:48
  */
