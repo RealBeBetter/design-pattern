ba5e0890c27ef12126552d0cb3391afb7485ad16
Real <swrely@qq.com>
2022-11-10 18:38:52 +0800

    docs: add strategy docs

--- src/strategy/strategy/Strategy.java
+++ src/strategy/strategy/Strategy.java
@@ -6,6 +6,13 @@ package strategy.strategy;
  */
 public interface Strategy {
 
+    /**
+     * 运算方法
+     *
+     * @param num1 数字1
+     * @param num2 数字2
+     * @return 运算结果
+     */
     int doOperation(int num1, int num2);
 
 }
