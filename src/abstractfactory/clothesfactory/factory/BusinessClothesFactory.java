2d18706c2134da7678d52b4b473d969a711bb36b
Real <swrely@qq.com>
2022-12-10 00:43:21 +0800

    feat: add abstract factory(clothes) Pattern

--- src/abstractfactory/clothesfactory/factory/BusinessClothesFactory.java
+++ src/abstractfactory/clothesfactory/factory/BusinessClothesFactory.java
@@ -10,10 +10,12 @@ import abstractfactory.clothesfactory.clothes.Shirt;
  * @since 2022/12/9 22:08
  */
 public class BusinessClothesFactory extends ClothesFactory {
+    @Override
     public Shirt createShirt() {
         return new BusinessShirt();
     }
 
+    @Override
     public Pants createPants() {
         return new BusinessPants();
     }
--- src/abstractfactory/clothesfactory/factory/FashionClothesFactory.java
+++ src/abstractfactory/clothesfactory/factory/FashionClothesFactory.java
@@ -10,10 +10,12 @@ import abstractfactory.clothesfactory.clothes.Shirt;
  * @since 2022/12/9 22:10
  */
 public class FashionClothesFactory extends ClothesFactory {
+    @Override
     public Shirt createShirt() {
         return new FashionShirt();
     }
 
+    @Override
     public Pants createPants() {
         return new FashionPants();
     }
